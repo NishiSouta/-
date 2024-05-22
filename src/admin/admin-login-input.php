24e7be73df7761e0d97db4d46d82dffe2261ac03
yusei888 <2201146@s.asojuku.ac.jp>
2024-05-22 10:47:27 +0900

    Update admin-login-input.php

--- src/admin/admin-login-input.php
+++ src/admin/admin-login-input.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="css/login.css">
     <link rel="stylesheet" href="css/reset.css" />
     <title>管理者ログイン画面</title> 
 </head>
 <body>
-        <p><img class="rogo" src="img/AGB.png" alt="写真" width="129" height="110"></p>
+        <p><img class="rogo" src="img/AGB.png" alt="写真" width="129" height="90"></p>
     <div id="center"><p><h2>管理者ログイン</h2></p></div>
 
 <form action ="admin-login-output.php" method="post">
