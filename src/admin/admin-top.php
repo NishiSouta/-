83d06871d3bf4993ad14c40f56626f276dd1eab2
yusei888 <2201146@s.asojuku.ac.jp>
2024-05-22 10:47:34 +0900

    Update admin-top.php

--- src/admin/admin-top.php
+++ src/admin/admin-top.php
@@ -1,1 +1,22 @@
-//管理者トップ画面
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/reset.css" />
+    <title>管理者トップ画面</title> 
+</head>
+<body>
+        <p><img class="rogo" src="img/AGB.png" alt="写真" width="200" height="180"></p>
+    <div id="center"><p><h2>テーマ</h2></p></div>
+
+    <div id="center"><a href="account-itiran.php">アカウント</a></div><!-- 中央よせ・枠組み（グレー）  -->
+
+
+    <div id="center"><button type="submit" class="rogu" name="name" value="value">ログイン</button></div><!-- 中央よせ・枠組み（グレー）  -->
+
+</form>
+<br>
+</body>
+</html>
