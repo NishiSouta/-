69966dc9fe726610bf0b9e94469ce04cedc55110
yusei888 <2201146@s.asojuku.ac.jp>
2024-05-29 10:59:20 +0900

    Update sinki_kakunin.php

--- src/user/sinki_kakunin.php
+++ src/user/sinki_kakunin.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <link rel="stylesheet" href="sinki_nyuuryoku.css">
+                <link rel="stylesheet" href="./css/sinki_nyuuryoku.css">
                 <title>新規登録確認画面</title>
             </head>
             <body>
