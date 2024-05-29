a8e79613dcefb07d9b8ad2d881d3316a87e17f78
t4gnu <133129091+t4gnu@users.noreply.github.com>
2024-05-29 09:46:25 +0900

    Update top.php

--- src/user/top.php
+++ src/user/top.php
@@ -11,17 +11,7 @@
 </head>
 <body>
     <?php require 'db-connect.php'; ?>
-    <header>
-        <a herf="top.php"><img src="img/AGB.png" class="logo"></a>
-        <form method="get" id="form" action="自分のサイトURL">
-            <div class="nes-field">
-                <label for="search_field"></label>
-                <input type="text" id="search_field" class="nes-input" placeholder="キーワードを入力">
-            </div>
-        </form>
-        <a class="nes-btn"  id="prof" href="#">プロフィール</a>
-        <a class="nes-btn" id="logout" href="logout.php">ログアウト</a>
-    </header>
+    <?php require 'header.php'; ?>
     <main>
         <h3>お気に入り</h3>
         <div class="flex_box">
