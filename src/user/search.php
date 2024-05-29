45188bb234bfea2917cca4764855824f2bd501e8
t4gnu <133129091+t4gnu@users.noreply.github.com>
2024-05-29 12:35:12 +0900

    Update search.php

--- src/user/search.php
+++ src/user/search.php
@@ -4,37 +4,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link href="https://unpkg.com/nes.css@latest/css/nes.min.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Press+Start+2P" rel="stylesheet">
+    <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/top.css">
+    <title>検索結果</title>
 </head>
 <body>
     <?php require 'db-connect.php'?>
     <?php require 'header.php'?>
     <main>
         <h3>検索キーワード：<?= $_POST['keyword'] ?></h3>
-        <div class="img_flex-box">
+        <div class="flex_box">
             <?php
                 if ( empty($_POST['keyword']) ) {
                         echo '<h3 class="nothing">検索キーワードを入力してください</h3>';
+                }else{
+                    $pdo=new PDO($connect,USER,PASS);
+                    $sql=$pdo->prepare('select * from Theme where theme_name like ?');
+                    $sql->execute(['%'.$_POST['keyword'].'%']);
+                    if( empty($sql->fetchAll()) ){
+                        echo '<h3 class="nothing">検索キーワードに該当するゲームがありません</h3>';
                     }else{
-                        $pdo=new PDO($connect,USER,PASS);
-                        $sql=$pdo->prepare('select * from Product where product_name like ? and product_id not in (select product_id from Puchase)');
+                        $sql=$pdo->prepare('select * from Theme where theme_name like ?');
                         $sql->execute(['%'.$_POST['keyword'].'%']);
-                        if( empty($sql->fetchAll()) ){
-                            echo '<h3 class="nothing">検索キーワードに該当する商品がありません</h3>';
-                        }else{
-                            $sql=$pdo->prepare('select * from Product where product_name like ? and product_id not in (select product_id from Puchase)');
-                            $sql->execute(['%'.$_POST['keyword'].'%']);
-                            foreach ($sql as $row){
-                                echo '<a href="detail.php?id=',$row['product_id'],'"><div class="img_flex-item">',
-                                        '<img src="../img/',$row['product_img'],'.jpg" alt="写真" width="180vw" height="240">',
-                                        $row['product_name'],
-                                    '</div></a>';
-                            }
+                        foreach ($sql as $row){
+                            echo '<a href="detail.php?id=',$row['theme_id'],'"><div class="flex_item">',
+                                    '<img src="img/',$row['Theme_jpg'],'.jpg" class="img_game" alt="写真">',
+                                    '<div class="game_title">',$row['theme_name'],"</div>",
+                                '</div></a>';
                         }
                     }
-                ?>
-            </div>
-        </main>
+                }
+            ?>
+        </div>
+    </main>
 </body>
 </html>
 <?php require 'footer.php'?>
