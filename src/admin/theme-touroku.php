e2a6633dab24e9db007de045fc0fdf0fe753e8d6
yusei888 <2201146@s.asojuku.ac.jp>
2024-06-03 12:46:23 +0900

    Update theme-touroku.php

--- src/admin/theme-touroku.php
+++ src/admin/theme-touroku.php
@@ -1,9 +1,11 @@
+<?php require 'db-connect.php'; ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="css/reset.css" />
     <title>掲示板テーマ登録画面</title> 
 </head>
@@ -12,9 +14,8 @@
     
 
    <!-- テーマの一覧表示  -->
-   <?php require 'db-connect.php'; ?>
-   
-   <h3>テーマ</h3>
+
+<main>
         <div class="flex_box">
             <?php
                 $pdo=new PDO($connect,USER,PASS);
@@ -22,19 +23,22 @@
                 $cnt = 0;
                 foreach($sql as $row){
                     if( $cnt>6 ){
+                        
                         break;
                     }
                     echo '<a href="detail.php?id=',$row['theme_id'],'"><div class="flex_item">',
-                            '<img src="img/',$row['Theme_jpg'],'.jpg" class="img_game" alt="写真">',
+                            '<img src="img/',$row['theme_jpg'],'.jpg" class="img_game" alt="写真">',
                             '<div class="game_title">',$row['theme_name'],"</div>",
                         '</div></a>';
                     $cnt++;
+                    
                 }
+                
         ?>
     </div>
-    <div id="center"><p><h2>テーマ</h2></p></div>
-    <div id="centerl"><a href="admin-top.php"class="btn">戻る</a></div> <!-- 戻る  -->
-    <div id="centerr"><a href="theme-"class="btn">登録</a></div> <!-- 登録  -->
-    
+    <!--<div id="center"><p><h2>テーマ</h2></p></div>-->
+    <div id="modoru-button"><a href="admin-top.php"class="btn">戻る</a></div> <!-- 戻る  -->
+    <div id="toroku-botton"><a href="admin-theme-touroku-input.php"class="btn">登録</a></div> <!-- 登録  -->
+            </main>
 </body>
 </html>
