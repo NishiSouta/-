db1dc2a006dc13c434995162c4156eef5f15105e
yusei888 <2201146@s.asojuku.ac.jp>
2024-06-06 12:41:39 +0900

    Update admin-theme-touroku-input.php

--- src/admin/admin-theme-touroku-input.php
+++ src/admin/admin-theme-touroku-input.php
@@ -8,11 +8,11 @@
 </head>
 <body>
     <form action="admin-theme-touroku-output.php" method="post" enctype="multipart/form-data">
-        <input type="file" id="img" name="img" accept="image/*" required><br><br>
-        <label for="name">テーマ名:</label>
-        <textarea id="name" name="name" required></textarea><br><br>
-        <input type="submit" value="登録">
-        <a href="admin-top.php">戻る</a>
+    <div id="file-button"><input type="file" id="img" name="img" accept="image/*" required></div><br><br>
+    <div id="theme-button"> <label for="name">テーマ名:</label>
+        <textarea id="name" name="name" required></textarea></div><br><br>
+        <div id="touroku-button"><input type="submit" value="登録"></div>
+        <div id="modoru-button"> <a href="admin-top.php">戻る</a></div>
     </form>
 </body>
 </html>
