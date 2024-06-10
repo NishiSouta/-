c01ed3cba4245b80af98580048514fd0f975a33a
eto-taisei <148730594+eto-taisei@users.noreply.github.com>
2024-06-10 11:43:17 +0900

    Update db-connect.php

--- src/user/db-connect.php
+++ src/user/db-connect.php
@@ -1,10 +1,15 @@
 <?php
-    const SERVER ='mysql304.phy.lolipop.lan';
-    const DBNAME ='LAA1516804-budou';
-    const USER ='LAA1516804';
-    const PASS ='pass1109';
+const SERVER ='mysql304.phy.lolipop.lan';
+const DBNAME ='LAA1516804-budou';
+const USER ='LAA1516804';
+const PASS ='pass1109';
 
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+$connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+/*try {
+    $pdo = new PDO($dsn, USER, PASS);
+    // この後のコードで $pdo を使用してデータベースを操作する
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+    // 接続に失敗した場合のエラーメッセージを表示するなどの処理を行う
+}*/
 ?>
-
-
