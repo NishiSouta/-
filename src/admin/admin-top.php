26a7b87df60bb6f728b81fca076819e3f0f5bc40
eto-taisei <148730594+eto-taisei@users.noreply.github.com>
2024-06-27 11:22:13 +0900

    Update admin-top.php

--- src/admin/admin-top.php
+++ src/admin/admin-top.php
@@ -8,20 +8,22 @@
     <title>管理者トップ画面</title> 
 </head>
 <body>
-
-        <p><img class="rogo" src="img/AGB.png" alt="写真" width="200" height="180"></p><!-- ロゴ  -->
-    <div class="header">
-            <button class="logout"><a href="admin-login-input.php"class="btn">ログアウト</a></div></button>
+    <div class="container">
+        <p><img class="rogo" src="img/AGB.png" alt="ロゴ" width="500" height="280"></p>
+        <div class="header">
+            <button class="logout"><a href="admin-login-input.php" class="btn">ログアウト</a></button>
         </div>
-    <div class="buttons">
-            <button class="btn"><a href="account-itiran.php"class="btn">アカウント</a></button>
-            <button class="btn"><a href="comment-itiran.php"class="btn">コメント</a></button>
+        <div class="buttons">
+            <button class="btn"><a href="account-itiran.php" class="btn">アカウント</a></button>
+            <button class="btn"><a href="comment-itiran.php" class="btn">コメント</a></button>
         </div>
-        <div class="theme">
+        <div id="theme">
             <h3>テーマ</h3>
-            <button class="btn"> <a href="theme-touroku.php"class="btn">登録</a></button>
-            <button class="btn"><a href="theme-delete.php"class="btn">削除</a></button>
         </div>
-</div>
+        <div class="buttons">
+            <button class="btn"><a href="theme-touroku.php" class="btn">登録</a></button>
+            <button class="btn"><a href="theme-delete.php" class="btn">削除</a></button>
+        </div>
+    </div>
 </body>
 </html>
