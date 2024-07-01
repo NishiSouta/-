e9c2dbf5a0354d63835215316730771e46b65142
yusei888 <2201146@s.asojuku.ac.jp>
2024-07-01 12:46:04 +0900

    Update admin-theme-touroku-input.php

--- src/admin/admin-theme-touroku-input.php
+++ src/admin/admin-theme-touroku-input.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/admin-theme-touroku-input.css">
     <title>テーマ登録画面</title>
 </head>
 <body>
     <form action="admin-theme-touroku-output.php" method="post" enctype="multipart/form-data">
-    <div id="file-button"><input type="file" id="img" name="img" accept="image/*" required></div><br><br>
+    <div id="file-button"><input type="file" id="img" name="img" accept="image/*"  onchange="previewUserIcon(event)" requiredtry></div><br><br>
     <div id="theme-button"> <label for="name">テーマ名:</label>
         <textarea id="name" name="name" required></textarea></div><br><br>
         <div id="touroku-button"><input type="submit" value="登録"></div>
