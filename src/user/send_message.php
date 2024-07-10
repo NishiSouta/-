45a44cd9aba436d5e8a904151e3b8d3296f8590e
t4gnu <133129091+t4gnu@users.noreply.github.com>
2024-07-10 12:03:32 +0900

    Update send_message.php

--- src/user/send_message.php
+++ src/user/send_message.php
@@ -1,17 +1,32 @@
 <?php session_start(); ?>
 <?php require 'db-connect.php'?>
 <?php
-    $pdo = new PDO($connect,USER,PASS);
+    if(empty($_POST['drone'])){
+        $pdo = new PDO($connect,USER,PASS);
 
-    $message = $_POST['message'];
-    $drone = $_POST['drone'];
+        $message = $_POST['message'];
+    
+        $message = $message;
+    
+        $sql = $pdo->prepare("INSERT INTO Chat(chat_postdate, chat_content, user_id, board_id) VALUES (NOW(),?,?,?)");
+        $sql->execute([$message, $_SESSION['user']['user_id'], $_GET['board_id']]);
+    
+        // チャットページにリダイレクト
+        header("Location: chat.php?board_id=".$_GET['board_id']);
+        exit;
+    }else{
+        $pdo = new PDO($connect,USER,PASS);
 
-    $message = $drone . $message;
-
-    $sql = $pdo->prepare("INSERT INTO Chat(chat_postdate, chat_content, user_id, board_id) VALUES (NOW(),?,?,?)");
-    $sql->execute([$message, $_SESSION['user']['user_id'], $_GET['board_id']]);
-
-    // チャットページにリダイレクト
-    header("Location: chat.php?board_id=".$_GET['board_id']);
-    exit;
+        $message = $_POST['message'];
+        $drone = $_POST['drone'];
+    
+        $message = $drone . $message;
+    
+        $sql = $pdo->prepare("INSERT INTO Chat(chat_postdate, chat_content, user_id, board_id) VALUES (NOW(),?,?,?)");
+        $sql->execute([$message, $_SESSION['user']['user_id'], $_GET['board_id']]);
+    
+        // チャットページにリダイレクト
+        header("Location: chat.php?board_id=".$_GET['board_id']);
+        exit;
+    }
 ?>
