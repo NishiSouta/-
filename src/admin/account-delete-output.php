25b9aac5263726645f7e72b31841f961f2776b60
SASAKI-NATSUKA <2201220@s.asojuku.ac.jp>
2024-07-17 09:56:25 +0900

    Update account-delete-output.php

--- src/admin/account-delete-output.php
+++ src/admin/account-delete-output.php
@@ -13,9 +13,24 @@
     <?php
 
     if (isset($_POST['user_id']) ) {
+        $user_id = $_POST['user_id'];
         $pdo=new PDO($connect, USER, PASS);
-        $sql=$pdo->prepare('delete from User where user_id=?');
-            $sql->execute([(int)$_POST['user_id']]);
+        $sql = 'DELETE FROM Favorite WHERE user_id = :user_id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+        $sql = 'DELETE FROM Chat WHERE user_id = :user_id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+        // ユーザーの削除
+        $sql = 'DELETE FROM User WHERE user_id = :user_id';
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
+
+            // $sql->execute([(int)$_POST['user_id']]);
 
             echo '削除完了しました。';
             echo '<br>';
