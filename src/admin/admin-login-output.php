520c49caaa51fc67a22a005008cf3c8e568fbbe9
yusei888 <2201146@s.asojuku.ac.jp>
2024-07-17 11:18:45 +0900

    Update admin-login-output.php

--- src/admin/admin-login-output.php
+++ src/admin/admin-login-output.php
@@ -13,7 +13,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css">
+    <link rel="stylesheet" href="css/login1.css">
     <link rel="stylesheet" href="css/reset.css" />
     <title>管理者ログイン画面</title>
 </head>
